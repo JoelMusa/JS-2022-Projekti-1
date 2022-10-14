2828e6df2565dbdea87275bb1ea09261cd1ce4c3
joelmusakka <91203662+joelmusakka@users.noreply.github.com>
2022-10-14 14:12:22 +0300

    Lisäys toiminto

---
+++ Projekti 1JS.js
@@ -0,0 +1,17 @@
+function    newListElement()    {
+    var list = document.getElementById('ul');
+    var input = document.getElementById('input').value;
+    var add = document.createElement('li');
+
+    if (input === '' || input.length < 3) {
+        alert("Tekstin tulee olla vähintään 3 merkkiä pitkä.");
+        
+      } else {
+       
+    add.appendChild(document.createTextNode(input));
+    list.appendChild(add);
+      } 
+      
+    document.getElementById("input").value = "";
+}
+
