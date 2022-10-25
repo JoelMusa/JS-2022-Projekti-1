a05839335469a21986ce2c85610b28f2735e5c41
joelmusakka <91203662+joelmusakka@users.noreply.github.com>
2022-10-25 16:48:46 +0300

    Muutoksia

--- Projekti 1JS.js
+++ Projekti 1JS.js
@@ -1,17 +1,33 @@
 function    newListElement()    {
     var list = document.getElementById('ul');
     var input = document.getElementById('input').value;
-    var add = document.createElement('li');
+    var addList = document.createElement('li');
+    var addCheckbox = document.createElement('input')
+    addCheckbox.type = "checkbox"
+    addCheckbox.id = "checkbox"
 
     if (input === '' || input.length < 3) {
         alert("Tekstin tulee olla vähintään 3 merkkiä pitkä.");
         
       } else {
-       
-    add.appendChild(document.createTextNode(input));
-    list.appendChild(add);
+    addList.appendChild(addCheckbox)  
+    addList.appendChild(document.createTextNode(input));
+    list.appendChild(addList);
+    
+    
       } 
-      
-    document.getElementById("input").value = "";
+    
+      document.getElementById("input").value = "";
 }
 
+var list = document.querySelector('ul');
+list.addEventListener('click', check(), uncheck())
+
+
+function check() {
+  document.getElementById("myCheck").checked = true;
+}
+
+function uncheck() {
+  document.getElementById("myCheck").checked = false;
+}
