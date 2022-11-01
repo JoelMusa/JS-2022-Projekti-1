d9679fa5af83233a7c36e3115bcdb21d8212d94e
Kananugetti <samiheinanen96@gmail.com>
2022-11-01 16:33:36 +0200

    Kokeilu

--- Projekti 1JS.js
+++ Projekti 1JS.js
@@ -1,25 +1,47 @@
-function    newListElement()    {
+function newListElement()    {
     var list = document.getElementById('ul');
     var input = document.getElementById('input').value;
-    var inputhistory = JSON.parse(localStorage.getItem("Tehtävät")) || [];
-    inputhistory.push(input);
-    localStorage.setItem("Tehtävät", JSON.stringify(inputhistory));
-
     var add = document.createElement('li');
     
-    
-
-    if (input === '' || input.length < 3) {
+   if (input === '' || input.length < 3) {
         alert("Tekstin tulee olla vähintään 3 merkkiä pitkä.");
         
-      } else {
+    } else {
        
     add.appendChild(document.createTextNode(input));
     list.appendChild(add);
-      } 
-      
+   
+    
+    } 
+
     document.getElementById("input").value = "";
+    
+    
 }
+
+function tallenna() {
+
+  var input = document.getElementById('input').value;
+  var old = JSON.parse(localStorage.getItem('tehtävät')) || []; 
+    old.push(input);
+    localStorage.setItem('tehtävät', JSON.stringify(old));
+
+
+}
+
+
+  
+
+  
+
+
+
+  
+
+
+
+
+
 
 
 
